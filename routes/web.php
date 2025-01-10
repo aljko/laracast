8b46fd5b83580e3908b9f89ede5520874da82781
aljko <aljkoroz@gmail.com>
2025-01-11 00:03:58 +0100

    Episode 10

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('posts');
 });
 
+Route::get('/cache', function () {
+    $cache = cache()->remember("user", 0.5, function() {
+        echo "dans le cache \n";
+        return "!! \n";
+    });
+    echo $cache;
+});
+
 Route::get('/posts/{post}', function ($slug) {
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
@@ -14,10 +22,13 @@ Route::get('/posts/{post}', function ($slug) {
         // abort(404);
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", 5, function () use ($path){
+        var_dump('mise en cache');
+        return file_get_contents($path);
+    });
 
     return view('post', [
         'post' => $post
     ]);
 })->where('post', '[A-z_/-]+');
-
+// })->whereAlpha('post');
